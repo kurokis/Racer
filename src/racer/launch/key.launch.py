667860936ea3c783023fd5c6aa54f817bda6e233
Shota Kuroki <kurokis@users.noreply.github.com>
2021-05-03 19:05:05 +0900

    Added keyboard control to launcher

--- src/racer/launch/key.launch.py
+++ src/racer/launch/key.launch.py
@@ -5,6 +5,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
     
+    keyboard_node = Node(
+        package='racer',
+        executable='keyboard',
+        # open another terminal for keyboard control
+        prefix = 'xterm -e',
+        output='screen',
+    )
+    
     key_ctl_node = Node(
         package='racer',
         executable='key_ctl',
@@ -16,6 +24,7 @@ def generate_launch_description():
         output='screen',
     )
     
+    ld.add_action(keyboard_node)
     ld.add_action(key_ctl_node)
     ld.add_action(s_motor_node)
     
