3cad3a8c1daf54a7349289d8b54f2f4a078b4b40
Shota Kuroki <kurokis@users.noreply.github.com>
2021-05-03 18:04:31 +0900

    Added throttle listener to launch file

--- src/racer/launch/key.launch.py
+++ src/racer/launch/key.launch.py
@@ -7,16 +7,16 @@ def generate_launch_description():
     
     key_ctl_node = Node(
         package='racer',
-            executable='key_ctl',
+        executable='key_ctl',
     )
     
-    #keyboard_node = Node(
-    #    package='racer',
-    #        executable='keyboard',
-    #        output='screen',
-    #)
+    s_motor_node = Node(
+        package='racer',
+        executable='s_motor',
+        output='screen',
+    )
     
     ld.add_action(key_ctl_node)
-    #ld.add_action(keyboard_node)
+    ld.add_action(s_motor_node)
     
     return ld
