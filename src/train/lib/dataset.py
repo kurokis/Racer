69497006f832de26c59c9ff4e9a1975e1059556a
kurokis <shota.kuroki.code@gmail.com>
2021-07-22 15:40:43 +0900

    Created a dataset class with sample data

---
+++ src/train/lib/dataset.py
@@ -0,0 +1,54 @@
+import glob
+from pathlib import Path
+import pandas as pd
+from PIL import Image
+import torch
+from torchvision import transforms
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, img_dir, labels_path):
+        img_paths_ = glob.glob(str(Path(img_dir)/"*.jpg"))
+        
+        column_names = ["filename","throttle","steer"]
+        df = pd.read_csv(labels_path, names=column_names)
+
+        img_paths = []
+        labels = []
+        for path in img_paths_:
+            df_match = df[df["filename"]==str(Path(path).name)]
+            if len(df_match)==0:
+                continue
+            throttle = df_match["throttle"].values[0]
+            steer = df_match["steer"].values[0]
+            label = [throttle, steer]
+            img_paths.append(path)
+            labels.append(label)
+        self.img_paths = img_paths
+        self.labels = labels
+
+        # define transforms to be applied on the image
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index):
+        path = self.img_paths[index]
+        image_pil = Image.open(path)
+        image = self.transform(image_pil)
+        label = torch.FloatTensor(self.labels[index])
+        return image, label
+
+# test code
+if __name__=="__main__":
+    dataset = MyDataset("../input_data","../input_data/labels.csv")
+    print(len(dataset))
+
+    # get the first element from the dataset
+    image, label = dataset[0]
+    print("image size:", image.shape)
+    print("image:", image)
+    print("label:", label)
