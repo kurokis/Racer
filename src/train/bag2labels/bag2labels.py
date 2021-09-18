113838758610296fc3c3d08f62c7b509f644cc7f
kurokis <shota.kuroki.code@gmail.com>
2021-09-18 10:58:25 +0900

    Data extraction from rosbag

--- src/train/bag2labels/bag2labels.py
+++ src/train/bag2labels/bag2labels.py
@@ -4,8 +4,8 @@ from rclpy.serialization import deserialize_message
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from PIL import Image
-
 
 class BagFileParser():
     def __init__(self, bag_file):
@@ -57,34 +57,63 @@ def get_camera_messages(parser):
     return np.array(t), images
 
 def sync_ts_and_images(t_ts, y_ts, t_im, y_im):
-    # t_ts: throttle_steerのタイムスタンプ配列
+    # t_ts: throttle_steerのタイムスタンプ配列(ナノ秒)
     # y_ts: throttle_steerの2次元配列
-    # t_im: imageのタイムスタンプ配列
+    # t_im: imageのタイムスタンプ配列(ナノ秒)
     # y_im: image(np.array)の配列
 
-    sample_rate = 10 # Hz
+    dt = 1 # seconds
 
-    # TODO: implement syncing
-    #t = t_ts[0]
-    #ts_indices = []
-    #for i in range(len(t_ts)):
-    #    if (t_ts[i]/10**9)>(t/10**9)+1.0/sample_rate:
-    #        # update timestamp on the ts side
-    #        t = t_ts[i]
-    #        ts_indices.append(i)
-    #print(ts_indices)
+    t_start = max(t_ts[0], t_im[0])
+    t_end = min(t_ts[-1], t_im[-1])
+    
+    n = int((t_end-t_start)/10**9 / dt)
+    n = max(n-1, 0) # subtract 1, but keep it non-negative
+    t = t_start
+    synchronized = [] # each row consists of timestamp, throttle_steer, image
+    for i in range(n):
+        # 時刻がt以上となる最初の位置を計算
+        i_ts = np.where(t_ts >= t)[0][0]
+        i_im = np.where(t_ts >= t)[0][0]
+
+        # その時刻のthrottle_steerとimageをセットとみなす
+        ts = y_ts[i_ts]
+        im = y_im[i_im]
+        synchronized.append([t,ts,im])
+
+        # 時刻を更新
+        t += dt*10**9
+
+    return synchronized
         
 if __name__ == "__main__":
-
-    #bag_file = '/workspaces/foxy_leg_ws/rosbag2_2021_04_28-04_42_07/rosbag2_2021_04_28-04_42_07_0.db3'
-    bag_files = glob.glob("input_data/**/*.db3")
+    bag_files = glob.glob(os.path.dirname(os.path.abspath(__file__))+"/input_data/**/*.db3")
     bag_file = bag_files[0]
     print(bag_file)
     parser = BagFileParser(bag_file)
 
+    print("Extracting throttle_steer")
     t_ts, y_ts = get_ts_messages(parser)
     print(t_ts)
 
+    print("Extracting images")
     t_im, y_im = get_camera_messages(parser)
 
-    sync_ts_and_images(t_ts, y_ts, t_im, y_im)
+    print("Synchronizing data")
+    synchronized = sync_ts_and_images(t_ts, y_ts, t_im, y_im)
+
+    print("Writing file")
+    output_dir = os.path.dirname(os.path.abspath(__file__))+"/output_data"
+    os.makedirs(output_dir, exist_ok=True)
+    for s in synchronized:
+        timestamp = s[0]
+        throttle = s[1][0]
+        steer = s[1][1]
+        image = s[2]
+        filename = str(s[0])+".jpg"
+        # write labels
+        with open(output_dir+"/labels.csv", "a") as f:
+            f.write("{},{},{}\n".format(filename,throttle,steer))
+        # write image
+        pil_img = Image.fromarray(image)
+        pil_img.save(output_dir+"/"+filename)
