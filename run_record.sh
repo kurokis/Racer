013c7809a61f1e8e7aff35c51520a1f7d5736f88
kurokis <kurokis@users.noreply.github.com>
2021-09-18 23:52:52 +0900

    Change current directory to /app/src before colcon build (run_record.sh)

--- run_record.sh
+++ run_record.sh
@@ -1,1 +1,1 @@
-colcon build --packages-select racer && . install/setup.bash && ros2 launch racer racer_record.launch.py
+cd "$(dirname "$0")" && cd src && colcon build --packages-select racer && . install/setup.bash && ros2 launch racer racer_record.launch.py
