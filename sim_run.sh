a88fc22553f346f355ae06d18630e8a6e2f06d44
kurokis <kurokis@users.noreply.github.com>
2021-09-18 22:43:31 +0900

    Change current directory to /app/src before colcon build

--- sim_run.sh
+++ sim_run.sh
@@ -1,1 +1,1 @@
-colcon build --packages-select racer && . install/setup.bash && ros2 launch racer sim_racer.launch.py
+cd "$(dirname "$0")" && cd src && colcon build --packages-select racer && . install/setup.bash && ros2 launch racer sim_racer.launch.py
