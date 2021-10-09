1be4c46d78e6686237abfb8f5a941d95bbdf1d22
Shota Kuroki <shota.kuroki.code@gmail.com>
2021-10-09 23:46:16 +0900

    Use random rotation (-5,5) degrees during training

--- src/train/train/lib/dataset.py
+++ src/train/train/lib/dataset.py
@@ -43,9 +43,18 @@ class MyDataset(torch.utils.data.Dataset):
         self.hflips = hflips
 
         # define transforms to be applied on the image
+        #transform = transforms.Compose([
+        #    transforms.ColorJitter(0.2, 0.2, 0.2, 0.2),
+        #    transforms.Resize((224, 224)), # for resnet
+        #    transforms.ToTensor(),
+        #    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        #])
+
+        # add random rotation
         transform = transforms.Compose([
             transforms.ColorJitter(0.2, 0.2, 0.2, 0.2),
             transforms.Resize((224, 224)), # for resnet
+            transforms.RandomRotation(degrees=(-5, 5)), # for resnet
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
